e68d9c49fbd19904232643f5a28855f3118a9302
pj <pj@CANON.williamson.home>
2021-11-03 23:35:08 -0400

    updated gui

--- MIPSIM.py
+++ MIPSIM.py
@@ -3,16 +3,22 @@ import PySimpleGUI as sg
 
 class fields:
 	def __init__(self):
-		self.ins = 'N'
-		self.op = 'N'
-		self.func = 'N'
-		self.rd = 'N'
-		self.rs = 'N'
-		self.rt = 'N'
-		self.imm = 'N'
-		self.result = 'N'
+		self.ins = 'U'
+		self.op = 'U'
+		self.func = 'U'
+		self.rd = 'U'
+		self.rs = 'U'
+		self.rt = 'U'
+		self.imm = 'U'
+		self.result = 'U'
 	def print_fields(self):
 		print('     ', self.ins, self.op, self.func, self.rd, self.rs, self.rt, self.imm)
+	def print_fields_string(self):
+		if self.ins == 'U':
+			return ''
+		if self.ins == 'NOP':
+			return '(NO OPERATION)'
+		return 'instruction: ' + str(self.ins) + '\n' + str(self.op) + ' ' + str(self.func) + ' ' + str(self.rd) + ' ' + str(self.rs) + ' ' + str(self.rt) + ' ' + str(self.imm)
 
 
 
@@ -266,14 +272,14 @@ def mem(E):
 
 
 
-def write_back(reg_dict, target, PC, M):
-
+def write_back(reg_dict, target, PC, M, window):
 	# note to pj: test flags
+	W = copy.deepcopy(M)
 	if M.ins == 'NOP':
 		z = False
 		v = False
 		result = 'X'
-		return PC, z, v
+		return PC, z, v, W
 	result = M.result
 	z = v = False
 	if result == 'none':
@@ -297,8 +303,9 @@ def write_back(reg_dict, target, PC, M):
 			v = False
 		reg_dict[target] = result
 	#reg_dict[target] = result
-		
-	return PC, z, v
+	window['-FLAGTEXT-'].update('Flags\n' + 'Zero: ' + str(z) + '\nOverflow: ' + str(v))
+	
+	return PC, z, v, W
 
 
 
@@ -309,121 +316,202 @@ def print_all_lines(line_to_print):
 		string = string + word + ' '
 	print(string)
 
-
-
-# setup
-reg_dict = dict([("$r%s" % x, 0) for x in range(4)]) 
-#print_RF(reg_dict)
-PC = -1
-F = fields()
-D = fields()
-E = fields()
-M = fields()
-user_input = ''
-i = 1
-z = v = R = False
-all_lines, all_labels = load_program_into_memory('test.s')
-
-
 # GUI
 #sg.theme('DarkTanBlue')
-REG_section = [[sg.Text('Registers', key='-REGTEXT-', background_color='white', size=(100,100), text_color='black')]]
-FLAG_section = [[sg.Text('Flags', key='-FLAGTEXT-', background_color='white', size=(100,50), text_color='black')]]
+s = (20, 10)
+st = (18,10)
+sw = (24, 4)
+REG_section = [[sg.Text('Registers', key='-REGTEXT-', background_color='white', size=s, text_color='black')]]
+FLAG_section = [[sg.Text('Flags\nZero: Unknown\nOverflow: Unknown', key='-FLAGTEXT-', background_color='white', size=st, text_color='black')]]
+F_section = [[sg.Text('Fetch', key='-FTEXT-', background_color='white', size=sw, text_color='black')]]
+D_section = [[sg.Text('Decode', key='-DTEXT-', background_color='white', size=sw, text_color='black')]]
+E_section = [[sg.Text('Execute', key='-ETEXT-', background_color='white', size=sw, text_color='black')]]
+M_section = [[sg.Text('Memory', key='-MTEXT-', background_color='white', size=sw, text_color='black')]]
+W_section = [[sg.Text('Write back', key='-WTEXT-', background_color='white', size=sw, text_color='black')]]
 
-layout = [[[sg.Text('Clock control')], 
-		[sg.Button('Step'), 
-		sg.Button('Run'), 
-		sg.VerticalSeparator(), 
-		sg.Column(REG_section, element_justification='c'),
-		sg.VerticalSeparator(), 
-		sg.Column(FLAG_section, element_justification='c')]]]
-		
-window = sg.Window('MIPSIM', layout,size=(400, 400), location=(600,330))
+layout = [[[sg.Text('Clock control')], [sg.Button('Step'), sg.Button('Step over'), 
+		sg.Button('Run'), sg.Button('Restart'), sg.VerticalSeparator(), sg.Column(REG_section, element_justification = 'c'), sg.Column(FLAG_section, element_justification = 'c'),
+		sg.Column(F_section, element_justification = 'c'), sg.Column(D_section, element_justification = 'c'), sg.Column(E_section, element_justification = 'c'),
+		sg.Column(M_section, element_justification = 'c'), sg.Column(W_section, element_justification = 'c')], sg.Text('STEP: 0', key='-STEP-'), sg.Text('', key='-FINISHED-')]]
 
+window = sg.Window('MIPSIM', layout, size=(1600, 300), location=(300,330))
 
-
-
-
-# RUN
-while PC < (len(all_lines)-2):
+def ask_window(R, RS, SO, window, reg_dict):
 	while True:
-		if R:
-			break
 		GUI_event, values = window.read()
+		if R:
+			return R, SO, GUI_event
+		if GUI_event == "Restart":
+			RS = True
+			return R, RS, SO, GUI_event
 		if GUI_event == "Run":
 			R = True
 			break
 		elif GUI_event == "Step":
 			break
+		elif GUI_event == "Step over":
+			SO = True
+			break
 		elif GUI_event == sg.WIN_CLOSED:
 			exit(0)
 	
-	lines_left = (len(all_lines)-1) - PC
+	update_window(window, reg_dict)
 	
-	#1
-	PC, F = fetch(PC, all_lines, F)
-
+	return R, RS, SO, GUI_event
 	
-	#2
-	D = decode(PC, all_lines, all_labels, F)
-	PC, F = fetch(PC, all_lines, F)	
-	
-	#3
-	
-	E = execute(reg_dict, D)
-	D = decode(PC, all_lines, all_labels, F)
-	PC, F = fetch(PC, all_lines, F)
-	
-	#4
-	target, M = mem(E)
-	E = execute(reg_dict, D)
-	D = decode(PC, all_lines, all_labels, F)
-	PC, F = fetch(PC, all_lines, F)
-	
-	#5
-	PC, z, v = write_back(reg_dict, target, PC, M)
-	target, M = mem(E)
-	E = execute(reg_dict, D)
-	D = decode(PC, all_lines, all_labels, F)
-	
-	#6
-	PC, z, v = write_back(reg_dict, target, PC, M)
-	target, M = mem(E)
-	E = execute(reg_dict, D)
-		
-	#7
-	PC, z, v = write_back(reg_dict, target, PC, M)
-	target, M = mem(E)
-	
-	#8
-	PC, z, v = write_back(reg_dict, target, PC, M)
-	
-	
-	if not R:
-		#print('\n      PC is', PC)
-		#print('\n      STEP', i, '\n') 
-		#all_print_fields(F, D, E, M)
-		#print_all_lines(all_lines[PC])
-		#print_all_lines(all_lines[PC+1])
-		#print_all_lines(all_lines[PC+2])
-		#print_all_lines(all_lines[PC+3])
-		window['-REGTEXT-'].update('Registers\n' + print_RF_string(reg_dict))
-		window['-FLAGTEXT-'].update('Flags\n' + 'Zero:' + str(z) + '\nOverflow:' + str(v))
-		#print('Zero:', z, '\nOverflow:', v)
-	
-	i += 1
-
-#print()
-
-if R:
-	#print('\n      FINAL\n')
-	#print_RF(reg_dict)
+def update_window(window, reg_dict):
 	window['-REGTEXT-'].update('Registers\n' + print_RF_string(reg_dict))
-	window['-FLAGTEXT-'].update('Flags\n' + 'Zero:' + str(z) + '\nOverflow:' + str(v))
-	#print('Zero:', z, '\nOverflow:', v, '\n')
-	while True:
-		GUI_event, values = window.read()
-		if GUI_event == sg.WIN_CLOSED:
-			break
+	window['-FTEXT-'].update('Fetch\n' + F.print_fields_string())
+	window['-DTEXT-'].update('Decode\n' + D.print_fields_string())
+	window['-ETEXT-'].update('Execute\n' + E.print_fields_string())
+	window['-MTEXT-'].update('Memory\n' + M.print_fields_string())
+	window['-WTEXT-'].update('Write back\n' + W.print_fields_string())
+
+def reset_text(window):
+	window['-REGTEXT-'].update('Registers\n')
+	window['-STEP-'].update('STEP: 0')
+	window['-FINISHED-'].update('')
+	window['-FLAGTEXT-'].update('Flags\nZero: Unknown\nOverflow: Unknown')
+	window['-FTEXT-'].update('Fetch\n')
+	window['-DTEXT-'].update('Decode\n')
+	window['-ETEXT-'].update('Execute\n')
+	window['-MTEXT-'].update('Memory\n')
+	window['-WTEXT-'].update('Write back')
+	
+while True:
+	# setup
+	REG_NUM = 8
+	reg_dict = dict([("$r%s" % x, 0) for x in range(REG_NUM)]) 
+	#print_RF(reg_dict)
+	PC = -1
+	F = fields()
+	D = fields()
+	E = fields()
+	M = fields()
+	W = fields()
+	user_input = ''
+	i = 1
+	z, v, R, SO, RS = (False,)*5
+	all_lines, all_labels = load_program_into_memory('test.s')
+	
+	# RUN
+	while PC < (len(all_lines)-2):
+		
+		lines_left = (len(all_lines)-1) - PC
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		window['-STEP-'].update('STEP: ' + str(i))
+		
+		#1
+		PC, F = fetch(PC, all_lines, F)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		#2
+		D = decode(PC, all_lines, all_labels, F)
+		PC, F = fetch(PC, all_lines, F)	
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		#3
+		E = execute(reg_dict, D)
+		D = decode(PC, all_lines, all_labels, F)
+		PC, F = fetch(PC, all_lines, F)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		#4
+		target, M = mem(E)
+		E = execute(reg_dict, D)
+		D = decode(PC, all_lines, all_labels, F)
+		PC, F = fetch(PC, all_lines, F)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		#5
+		PC, z, v, W = write_back(reg_dict, target, PC, M, window)
+		target, M = mem(E)
+		E = execute(reg_dict, D)
+		D = decode(PC, all_lines, all_labels, F)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		#6
+		PC, z, v, W = write_back(reg_dict, target, PC, M, window)
+		target, M = mem(E)
+		E = execute(reg_dict, D)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+			
+		#7
+		PC, z, v, W = write_back(reg_dict, target, PC, M, window)
+		target, M = mem(E)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		#8
+		PC, z, v, W = write_back(reg_dict, target, PC, M, window)
+		if not SO and not R:
+			R, RS, SO, GUI_event = ask_window(R, RS, SO, window, reg_dict)
+			if RS:
+				reset_text(window)
+				RS = False
+				break
+		
+		i += 1
+			
+		SO = False
+	
+	if R or SO:
+		update_window(window, reg_dict)
+		window['-FLAGTEXT-'].update('Flags\n' + 'Zero: ' + str(z) + '\nOverflow: ' + str(v))
+		window['-FINISHED-'].update('File finished running.')
+		window.find_element('Step').Update(disabled=True)
+		window.find_element('Step over').Update(disabled=True)
+		window.find_element('Run').Update(disabled=True)
+		while True:
+			GUI_event, values = window.read()
+			if GUI_event == sg.WIN_CLOSED:
+				exit(0)
+			elif GUI_event == "Restart":
+				reset_text(window)
+				window.find_element('Step').Update(disabled=False)
+				window.find_element('Step over').Update(disabled=False)
+				window.find_element('Run').Update(disabled=False)
+				break
+			
 	
 exit(0)
--- gui tests/1.py
+++ gui tests/1.py
@@ -1,12 +1,13 @@
 import PySimpleGUI as sg
 
-layout = [ [sg.Text('My layout', key='-TEXT-')],
-           [sg.Button('Read')]]
+s = (20, 10)
+REG_section = [[sg.Text('Registers', key='-REGTEXT-', background_color='white', size=s, text_color='black')]]
+FLAG_section = [[sg.Text('Flags', key='-FLAGTEXT-', background_color='white', size=s, text_color='black')]]
 
-window = sg.Window('My new window', layout)
+layout = [[[sg.Text('Clock control')], sg.Button('Step'), 
+		sg.Button('Run'), sg.VerticalSeparator(), sg.Column(REG_section, element_justification = 'c'), sg.Column(FLAG_section, element_justification = 'c')]]
 
-while True:             # Event Loop
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
-    window['-TEXT-'].update('My new text value')
+window = sg.Window('MIPSIM', layout, size=(400, 400), location=(600,330))
+event, values = window.read()
+
+window.close()
