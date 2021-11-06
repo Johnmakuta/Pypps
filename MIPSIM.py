2cd7025e21616146f89670ed41b989603e9d84bf
pj <pj@CANON.williamson.home>
2021-11-06 01:18:40 -0400

    made it compatible with comments

--- MIPSIM.py
+++ MIPSIM.py
@@ -50,12 +50,14 @@ def load_program_into_memory(file_name):
 	all_lines, line, dummy_line = [], [], []
 	all_labels, memory = {}, {}
 	H, data_mode = False, False
+	target = '#'
 	with open(file_name) as test_file:
 		for file_line in test_file:
 			if file_line.startswith('#') or file_line.startswith(';') or file_line == '\n' or file_line == '':
 				pass
 			else:
 				file_line = file_line.replace(',', '')
+				file_line = file_line.split(';', 1)[0]
 				file_line = file_line.split()
 				line.append(file_line)
 			
